003e8d6b6873895798828422a506479023c49793
DuvanEZ <duvanminos@gmail.com>
2023-04-18 14:58:43 -0500

    Cambio en comentario

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -1,6 +1,7 @@
 package co.edu.eafit.protobootapp;
 
-//This is a simple calculator class.
+//This is a simple calculator class. 
+// This is not that simple
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
